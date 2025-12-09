9ec83d0cd78d4a484db89a05f8c66aafe35bd2a8
Ankit-Pdl <pdlankit2000@gmail.com>
2025-12-09 17:52:50 +0545

    second commit

---
+++ Basics/conditions.py
@@ -0,0 +1,33 @@
+# if statement
+# if condition:
+# age = 30
+# if age>= 21:
+#   print("hello world")
+
+# if else
+
+# age = 27
+# if age==27:
+#   print('age is 27')
+# else:
+#   print("age is not 27")  
+# score = 44
+# if score>=90:
+#   print("good")
+# elif score>=35:
+#   print("average")
+# else:
+#   print('hello world')    
+# day = 'Sunday'
+# is_holiday:True
+
+# if day == "Sunday" or day == "sat" or is_holiday:
+# #   print("weekend")
+# # else:
+# #   print("not a weekend")  
+
+# value = 0
+# if value:
+#   print("A")
+# else:
+#   print("B")  
---
+++ loops.py
@@ -0,0 +1,105 @@
+# for loop
+
+# fruits = ['a','b','k']
+
+# for fruit in fruits:
+#   print(fruit)
+
+# range function
+
+# for i in range(1,6):
+#   print(i)
+
+# while loop
+
+# count =1
+# while count<=5:
+#   print(count)
+#   count += 1
+
+# loop control statement
+
+# for letter in "Anamika":
+#   if letter  == 'k':
+#     break
+#   print(letter)
+
+
+
+# for i in range(1,6):
+#   if i == 4:
+#     continue
+#   print(i)
+
+# for item in ['A','B','C']:
+#   if item == 'B':
+#     pass
+#   else:
+#     print(item)
+
+# for i in range (1,11):
+#    print(f'34 * {i}: {i*34}')
+
+
+# N = int(input("enter a num\t"))
+# N+=1
+# sum = 0
+# for i  in range(N):
+#    sum += i
+
+
+# print(sum)
+
+# string = 'anamika'
+# for i  in string:
+#   print(i)
+
+# count = 0
+# string = 'anamihdjfbweifnaksaJBWJSDCQBFAAAADV'
+# for i in string.lower():
+#   if i == 'a':
+#     count +=1
+
+# print(count)
+
+# for i in range(1,101):
+#   if i%3 == 0 and i%5 ==0:
+#     print(i)
+# result  = []
+# for i in 'ankit':
+#    result.insert(0,i);
+# print(result)
+
+# s= 'yunisha'
+# rev = ''
+
+# for ch in s:
+#   rev = ch +rev
+
+# print(rev)
+
+# lists = [1,2,3,4,5]
+# max = list[0]
+# for items in list:
+#    if items> max:
+#       max_num = items
+
+# print(max_num)
+
+# print(max(lists))
+
+# count =0
+# num = 12345
+# for i in str(num):
+#   count+=1
+
+# print(count)  
+ 
+
+num = 343754343433
+count = 0
+
+while num!=0:
+  num = num //10
+  count+=1
+print(count)  
