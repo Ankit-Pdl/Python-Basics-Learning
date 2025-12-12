988aca4dea120e7a16abb471eb48ccc6a6b44575
Ankit-Pdl <pdlankit2000@gmail.com>
2025-12-12 21:19:41 +0545

    Learned functions professionally

--- Basics/_string_operations.py
+++ Basics/_string_operations.py
@@ -53,92 +53,94 @@
 # string = 'Anamika is not a good girl'
 # print(string.split(' '))
 
-# name = '.'.join(['Ankit','Poudel'])
-# print(name)
+
+# # name = '.'.join(['Ankit','Poudel'])
+# # print(name)
 
 
-# string = ' hello from the other sideee '
-# print(string.strip())
+# # string = ' hello from the other sideee '
+# # print(string.strip())
 
-# print(string.lstrip())
+# # print(string.lstrip())
 
-# string = 'Nepal'
-# print(string.isalpha())
-# print(string.isdigit())
-# print(string.isalnum())
-# print(string.islower())
-# print(string.isupper())
+# # string = 'Nepal'
+# # print(string.isalpha())
+# # print(string.isdigit())
+# # print(string.isalnum())
+# # print(string.islower())
+# # print(string.isupper())
 
-# name = 'Anamika'
-# age= 22
-# print(f'my name is {name} and I am {'twenty-one' if age==21 else 'dont know'} years old')
-
-
-# print(ord('A'))
-# print(chr(126))
+# # name = 'Anamika'
+# # age= 22
+# # print(f'my name is {name} and I am {'twenty-one' if age==21 else 'dont know'} years old')
 
 
-# import time
-# from strings import text
-# for char in text:
-#   print(char,end="", flush=True)
-#   time.sleep(0.09)
-
-# name = 'hello'
-# print(name[::-1])
-# reversed_string = ''
-# name = 'hello from the other side'
-# for char in name:
-#   reversed_string = char + reversed_string
-# print(reversed_string)  
-# empty = []
-# ascii=''
-# from strings import text
-# count = 0
-# vowels = 'aeiouAEIOU'
-# # user_Input = input("enter any string ")
-# for char in text:
-#   if char in vowels:
-#     ascii = ord(char)
-#     empty.append(ascii)
-
-# result = list(set(empty))
-# result.sort()
-
-# print(result)
-
-# from strings import text
-# vowels = 'aeiouAEIOU'
-# ascii_values = sorted({ord(char) for char in text if char in vowels})
-# print(ascii_values)
-
-# string = input("enter a string ")
-# result = string.replace(' ', '')
-# print(result)
-
-# find the most frequent character in a string
-
-# text = input("Enter a string:")
-# freq = {}
-# print(type(freq))
-# for char in text:
-#   freq[char] = freq.get(char, 0)+1
-
-# most_frequent = max(freq,key=freq.get)
-# print("Most frequent character: ", most_frequent)
-# print("Frequency: ",freq[most_frequent])
+# # print(ord('A'))
+# # print(chr(126))
 
 
-# def most_frequent_char_max(s):
-#     # Iterate over unique characters to avoid redundant counting
-#     if not s:
-#         return None, 0
+# # import time
+# # from strings import text
+# # for char in text:
+# #   print(char,end="", flush=True)
+# #   time.sleep(0.09)
+
+# # name = 'hello'
+# # print(name[::-1])
+# # reversed_string = ''
+# # name = 'hello from the other side'
+# # for char in name:
+# #   reversed_string = char + reversed_string
+# # print(reversed_string)  
+# # empty = []
+# # ascii=''
+# # from strings import text
+# # count = 0
+# # vowels = 'aeiouAEIOU'
+# # # user_Input = input("enter any string ")
+# # for char in text:
+# #   if char in vowels:
+# #     ascii = ord(char)
+# #     empty.append(ascii)
+
+# # result = list(set(empty))
+# # result.sort()
+
+# # print(result)
+
+# # from strings import text
+# # vowels = 'aeiouAEIOU'
+# # ascii_values = sorted({ord(char) for char in text if char in vowels})
+# # print(ascii_values)
+
+# # string = input("enter a string ")
+# # result = string.replace(' ', '')
+# # print(result)
+
+# # find the most frequent character in a string
+
+# # text = input("Enter a string:")
+# # freq = {}
+# # print(type(freq))
+# # for char in text:
+# #   freq[char] = freq.get(char, 0)+1
+
+# # most_frequent = max(freq,key=freq.get)
+# # print("Most frequent character: ", most_frequent)
+# # print("Frequency: ",freq[most_frequent])
+
+
+# # def most_frequent_char_max(s):
+# #     # Iterate over unique characters to avoid redundant counting
+# #     if not s:
+# #         return None, 0
         
-#     most_common_char = max(set(s), key=s.count)
-#     frequency = s.count(most_common_char)
-#     return most_common_char, frequency
+# #     most_common_char = max(set(s), key=s.count)
+# #     frequency = s.count(most_common_char)
+# #     return most_common_char, frequency
 
-# # Example Usage:
-# input_string = "speed demon"
-# char, count = most_frequent_char_max(input_string)
-# print(f"The most frequent character is '{char}' with a count of {count}.")
+# # # Example Usage:
+# # input_string = "speed demon"
+# # char, count = most_frequent_char_max(input_string)
+# # print(f"The most frequent character is '{char}' with a count of {count}.")
+
--- Basics/functions.py
+++ Basics/functions.py
@@ -139,18 +139,240 @@
 
 
 
-def greet(name):
-  return f"Hello {name}"
+# def greet(name):
+#   return f"Hello {name}"
 
-def excited_wrapper(func):
-  def wrapper(*args,**kwargs):
-    original = func(*args,**kwargs)
-    return original.upper() + "😁"
-  return wrapper
+# def excited_wrapper(func):
+#   def wrapper(*args,**kwargs):
+#     original = func(*args,**kwargs)
+#     return original.upper() + "😁"
+#   return wrapper
 
-greet = excited_wrapper(greet)
-print(greet("Ankit"))
+# greet = excited_wrapper(greet)
+# print(greet("Ankit"))
+
+# logging decorator
+
+
+# def log(func):
+#   def wrapper(*args,**kwargs):
+#     print(f"calling {func.__name__}")
+#     return func(*args,**kwargs)
+#   return wrapper
+
+# @log
+# def add(a,b):
+#   return a+b
+
+# print(add(4,5))
+
+# def square(args):
+#    return args**2
+
+# print(square(5))
+
+# def addTwoNumbers(a,b):
+#   return a+b
+
+# print(addTwoNumbers(34,45))
+
+
+# def checkEvenStatus(number):
+#   if number%2==0:
+#     return f"{number} is even"
+#   else :
+#     return f"{number} is odd"
+# print(checkEvenStatus(23)  )
+
+#program that takes a list and returns a largest element
+
+# def findLargest(userList):
+#   x= userList[0]
+#   for i in userList:
+#     if i>x:
+#       x=i
+#   return x
+
+# checkNum= [1,2,3,64,56,67,123] 
+
+# def findLargest(lst):
+#   return max(lst)
+
+# print(findLargest(checkNum))  
+
+
+# def inRever(userString):
+#   return "".join(reversed(userString))
+
+# string = "Anamika"
+# print(inRever(string))
+
+
+
+# def inRever(input):
+#   reversed_output = ''
+#   for i in input:
+#     reversed_output =  i+ reversed_output
+#   return reversed_output
+  
+# print(inRever("ankit"))  
+
+# userNumber = int(input("Enter a number maximum upto 100 "))
+# def calcFactorial(number):
+#   fact = 1
+#   if number>=0:
+#     while number!= 1:
+#      if number == 0 or number == 1:
+#        return fact
+#      fact = fact *number
+#      number-=1
+#   else : 
+#     print("number cannot be negative ")
+#   return fact
+
+
+# print(f'Factorial of {userNumber} is {calcFactorial(userNumber)}')
+
+# now a good practice
+# RED = "\033[91m"
+# RESET = "\033[0m"
+# def calcFactorial(number):
+#   if number<0:
+#     print( RED + "Error: Negative number not allowed"+RESET)
+#     return None
+#   fact = 1
+#   for i in range(2,number+1):
+#     fact*=i
+#   return fact
+
+# userNumber = int(input("Enter a number: "))
+# result  = calcFactorial(userNumber)
+
+# if result is not None:
+#   print(f'Factorial of {userNumber} is {result}')
+
+
+# def calcFactorial(number):
+#   if number<0 :
+#     raise ValueError("Number cannot be negative ")
+#   else:
+#     fact = 1
+#     for i in range(2,number+1):
+#       fact*=i
+#   return fact
+
+# inp = int(input("Enter a number "))
+# print(calcFactorial(inp))     
+
+
+# def intro(name,age):
+#   return f"My name is {name} and I am {age}"
+
+# name = input("Enter a name:  ")
+# try:
+#    age = int(input("Enter your age: "))
+# except ValueError:
+#    print("Invalid input! Age must be a number.")
+#    exit()
+
+
+
+# print(intro(name,age))
+
+# a better way to do the upper code :::
+
+
+# def intro(name:str,age:int) ->str:
+#   """Retrun a formatted introduction message"""
+#   return f" My name is {name} and I am {age} years old."
+
+# def get_valid_age()->int:
+#   """Continuously prompt the user until they enter a valid integer age."""
+#   while True:
+#     age_input = input("Enter your age: ").strip() # strip filters the string
+#     if age_input.isdigit():
+#       return int(age_input)
+#     print("\033[91mError: Please enter a valid number for age.\033[0m")
+
+# def main():
+#   name = input("Enter your name: ").strip().title().upper()
+#   age = get_valid_age()
+#   print(intro(name,age))
+
+# if __name__ == "__main__":
+#      main()
+
+
+# program that takes a list of number and returns a list with only even numbers
+
+
+# userIterate = int(input("Enter number of items you want : "))
+# userList = []
+# for i in range(0,userIterate):
+#  userList.append(int(input(f"Enter item : ")) ) 
+
+#  new_list =[x for x in userList if x%2==0]
+
+# print(new_list)
+
+# a better way:
+
+# def get_even_number() :
+#   """ Collects numbers from user and return a list of even numbers"""
+
+#   try:
+#     n = int(input("how many numbers would you like to enter? "))
+#   except ValueError:
+#     print("Please enter a valid number! ")
+#     return []
+
+#   numbers = []
+#   for _ in range(n):
+#     while True:
+#       try:
+#         num = int(input("Enter a number"))
+#         numbers.append(num)
+#         break
+#       except ValueError:
+#         print("Invalid Input. Please enter an integer.")
+
+#   even_numbers = [num for num in numbers if num%2 ==0]
+#   return even_numbers
+
+
+
+# if __name__ == "__main__":
+  
+#   evens = get_even_number()
+#   print("Even numbers:",evens)
+
+# program to find the number of iterations of an element in a list
+# userList = []
+# n = int(input("Enter iterations: "))
+# while n!=0:
+#  userList.append(int(input("Enter the elements in the list ")))
+#  n-=1
+# def count_iteration(userList):
+#     checkUser = int(input("tell me which number you want to check:"))
+#     count = 0
+#     for i in userList:
+#        if i == checkUser:
+#           count+=1
+#     return count
+
+# print(f"Your choosen number has been repeated { count_iteration(userList) } times")
+
+# much better way:
+
+def count_iteration(lst,number):
+  """Counts how many times 'number' occurs in 'list'"""
+  return lst.count(number)
+
+userList = [int(input("Enter element: ")) for _ in range(int(input("How many numbers?")))]
 
 
 
 
+if __name__ == "__main__":
+  nums_to_check = int(input("Which number do you want to check?"))
+  print(f"The number {nums_to_check} has been repeated {count_iteration(userList,nums_to_check)} times.")
